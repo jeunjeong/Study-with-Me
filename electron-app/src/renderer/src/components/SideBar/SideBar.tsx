8e71f017427e54c91cd23c2a60e6f79b98590ec3
eunjeong <ejjeongs@naver.com>
2024-05-13 18:08:07 +0900

    ✨ feat : Add sidebar UI

--- electron-app/src/renderer/src/components/SideBar/SideBar.tsx
+++ electron-app/src/renderer/src/components/SideBar/SideBar.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Container,
+  GroupSection,
+  GroupsItem,
+  GroupsImg,
+  SettingSection,
+  SettingItem,
+  SettingImg
+} from './Style'
+import todoicon from './todo-icon.png'
+import charticon from './chart-icon.png'
+import settingicon from './setting-icon.png'
+import icon from './icon.png'
 
-import './SideBar.css'
+function SideBar(): JSX.Element {
+  // useState를 사용하여 groups 상태와 이를 업데이트하는 setGroups 함수를 받습니다.
+  const [groupselected, setGroupSelected] = useState(0)
+  const [groups] = useState([icon, icon, icon, icon, icon, icon])
 
-function SideBar() {
+  const selectGroup = (index: number): void => {
+    setGroupSelected(index)
+  }
+
   return (
     <React.Fragment>
-      <div className="sidebar-container">This is Sidebar</div>
+      <Container className="sidebar-container">
+        <GroupSection>
+          {groups.map((group, index) => (
+            // isSelected와 같은 boolean 값을 GroupsItem 컴포넌트에 prop으로 전달합니다.
+            <GroupsItem
+              isSelected={index == groupselected}
+              key={index}
+              onClick={() => selectGroup(index)}
+            >
+              <GroupsImg src={group} alt={`Group ${index}`}></GroupsImg>
+            </GroupsItem>
+          ))}
+        </GroupSection>
+        <SettingSection>
+          <SettingItem>
+            <SettingImg src={todoicon} alt={`todo-icon`}></SettingImg>
+          </SettingItem>
+          <SettingItem>
+            <SettingImg src={charticon} alt={`chart-icon`}></SettingImg>
+          </SettingItem>
+          <SettingItem>
+            <SettingImg src={settingicon} alt={`setting-icon`}></SettingImg>
+          </SettingItem>
+        </SettingSection>
+      </Container>
     </React.Fragment>
   )
 }
---
+++ electron-app/src/renderer/src/components/SideBar/Style.tsx
@@ -0,0 +1,84 @@
+import { css } from '@emotion/react'
+import tw, { styled } from 'twin.macro'
+
+interface GroupsItemProps {
+  isSelected: boolean
+}
+
+export const Container = styled.div(() => [
+  tw`
+  `,
+  css`
+    width: 40px;
+    min-height: 100vh; /* 뷰포트의 100% 높이 */
+    max-height: 100%; /* 부모 요소의 높이를 초과하지 않음 */
+    background-color: black;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  `
+])
+export const GroupSection = styled.div(() => [
+  tw`
+  `,
+  css`
+    margin-top: 15px;
+    width: 100%;
+  `
+])
+export const GroupsItem = styled.div<GroupsItemProps>(({ isSelected }) => [
+  tw`
+  `,
+  css`
+    width: 40px;
+    aspect-ratio: 1 / 1;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    ${isSelected &&
+    css`
+      background-color: white;
+    `}
+  `
+])
+
+export const GroupsImg = styled.img(() => [
+  tw`
+  `,
+  css`
+    border-radius: 10px;
+    width: 80%;
+  `
+])
+
+export const SettingSection = styled.div(() => [
+  tw`
+  `,
+  css`
+    width: 100%;
+    margin-bottom: 15px;
+  `
+])
+
+export const SettingItem = styled.div(() => [
+  tw`
+  `,
+  css`
+    width: 100%;
+    aspect-ratio: 1 / 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `
+])
+export const SettingImg = styled.img(() => [
+  tw`
+  `,
+  css`
+    width: 80%;
+  `
+])
