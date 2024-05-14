9f3e71f1fcc52f5199dccd743a101392f3b0e1d7
eunjeong <ejjeongs@naver.com>
2024-05-14 15:13:42 +0900

    ✨ feat : Set recoil root

--- electron-app/src/renderer/src/main.tsx
+++ electron-app/src/renderer/src/main.tsx
@@ -1,14 +1,15 @@
 // src/main.tsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import GlobalStyles from './styles/GlobalStyles'
+import { RecoilRoot } from 'recoil'
 import App from './App'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 )
