02f05d1cd09ec5fc3c1f96418887f4350acc1725
eunjeong <ejjeongs@naver.com>
2024-05-14 15:11:08 +0900

    ✨ feat : Add main/group empty component

---
+++ electron-app/src/renderer/src/components/Group/GroupPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from './Style'
+function GroupPage(): JSX.Element {
+  return (
+    <React.Fragment>
+      <Container>여기는 그으으으룹</Container>
+    </React.Fragment>
+  )
+}
+
+export default GroupPage
