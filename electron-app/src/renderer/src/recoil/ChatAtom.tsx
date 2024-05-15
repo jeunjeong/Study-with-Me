d9bc0a3e1b4e5a423b4256fa32c8a4c325176cbc
jjeoungS2 <un2642@naver.com>
2024-05-15 14:54:42 +0900

    📝 Docs: 파일명수정

--- electron-app/src/renderer/src/recoil/ChatAtom.tsx
+++ electron-app/src/renderer/src/recoil/ChatAtom.tsx
@@ -1,6 +1,6 @@
 import { atom } from 'recoil'
 
 export const chatRoomState = atom({
-  key: 'chatRoomState', // 고유한 key
-  default: false // 기본값은 닫혀있음을 나타냄
+  key: 'chatRoomState',
+  default: false
 })
